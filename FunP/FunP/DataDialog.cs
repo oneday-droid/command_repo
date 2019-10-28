112e02aa2484de6021cf5636197d2fe3dae61278
oneday-droid <yugova.anastasia@gmail.com>
2019-10-28 17:49:53 +0500

    Add some interface (buttons and dialog form)

---
+++ FunP/FunP/DataDialog.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FunP
+{
+    public partial class DataDialog : Form, IDialogView
+    {
+        public DataDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void applyButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    
+        public void SetDataLabels(List<string> list)
+        {
+            int count = list.Count;
+            TextBox[] textBoxes = new TextBox[count];
+            Label[] labels = new Label[count];
+
+            for (int k = 0; k < count; k++)
+            {
+                labels[k] = new Label();
+                labels[k].Text = list[k];
+                flowLayoutPanel.Controls.Add(labels[k]);
+
+                textBoxes[k] = new TextBox();
+                flowLayoutPanel.Controls.Add(textBoxes[k]);                
+            }
+        }
+    }
+}
--- FunP/FunP/FunP.cs
+++ FunP/FunP/FunP.cs
@@ -16,5 +16,33 @@ namespace FunP
         {
             InitializeComponent();
         }
+
+        private void getDataButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void editButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            //здесь надо вытягивать список полей из презентера или модели
+            List<string> list = new List<string>();
+            list.Add("name");
+            list.Add("age");
+            list.Add("faculty");
+            DataDialog dialog = new DataDialog();
+            dialog.Text = "Add data";
+            dialog.SetDataLabels(list);
+            dialog.ShowDialog();
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
